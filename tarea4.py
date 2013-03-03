b012b8ce18c3ae36e8dbf1d2ab2f726412199e66
Andrea Rozo <andrea@andrea-530U3BI-530U4BI-530U4BH.(none)>
2013-03-02 19:08:49 -0500

    Matriz G

--- tarea4.py
+++ tarea4.py
@@ -5,12 +5,12 @@
 #	Andres Felipe Garcia Albarracin
 #	Andrea Rozo Mendez
 
-# Importa la librería numpay
-import numpay as np
+# Importa la libreria numpay
+import numpy as np
 
 # Leer un archivo
-# @Param: nombre del archivo
-# @Return: matriz de los datos leidos
+# @param: nombre del archivo
+# @return: matriz de los datos leidos
 def leer(nombreArchivo):
 	infile = open(nombreArchivo,'r')
 	text = infile.readlines()
@@ -28,13 +28,23 @@ def leer(nombreArchivo):
 
 # Crea la matriz de la regresion G
 # @param: matriz leida en el archivo
-
+# @return: matriz G para la regresion
+def matrizG(matrix):
+	m = [[1,matrix[0],0.5*matrix[0]*matrix[0]]]
+	i = 3
+	G = np.matrix(m)
+	while i < (len(matrix))-3:
+		m = [[1,matrix[i],0.5*matrix[i]*matrix[i]]]
+		G = np.concatenate((G,m))
+		i=i+3
+	return G
 
 # Hace la regresion
 # @param: matriz con los datos
 # @return: vector con [g, voy, vox]
-def regresion 
+
 
 
 matriz1 = leer("experimentID_0_theta_0.0.txt")
-print matriz1
+matriz2 = matrizG(matriz1)
+print matriz2
