b754f463e36ff8878fb512bd70fbfde8b9bd6f53
andresfgarcia150 <andrespipe150@gmail.com>
2013-03-06 22:45:34 -0500

    Punto 5

--- tarea4Andres2.py
+++ tarea4Andres2.py
@@ -10,17 +10,30 @@ print "f0 = ", f0
 print "c0 = ", c0
 fest = np.matrix(v1*f0+c0)
 fest = np.transpose(fest)
+error = f - fest
+print error
 
 # Grafica de salida
 import matplotlib.pyplot as plt
 from scipy.interpolate import interp1d
 
-plt.plot(angulos,f)
-plt.plot(angulos,fest)
-plt.legend(['Datos','Estimado'])
-ax = plt.axes()
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.plot(angulos,f)
+ax.plot(angulos,fest)
+ax.legend(['Datos','Estimado'])
 ax.set_xlabel("$\\theta$",fontsize=20)
 ax.set_ylabel("$F(\\theta)$",fontsize=20)
 ax.set_title("$\mathrm{Funcion\ F\ real\ y\ estimada}$", fontsize=20)
+filename = 'GraficaAjuste'
+fig.savefig(filename + '.jpeg',format = 'jpeg', transparent=True)
+
+
+fig2 = plt.figure()
+ax2 = fig2.add_subplot(111)
+ax2.bar(angulos,error)
+ax2.set_xlabel("$\\theta$",fontsize=20)
+ax2.set_ylabel("$E(\\theta)$",fontsize=20)
+ax2.set_title("$\mathrm{Error\ de\ F}$", fontsize=20)
 filename = 'GraficaResiduos'
-plt.savefig(filename + '.jpeg',format = 'jpeg', transparent=True)
+fig2.savefig(filename + '.jpeg',format = 'jpeg', transparent=True)
